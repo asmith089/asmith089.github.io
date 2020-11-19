ebce7399fd8c92c6129ee28f7e6652c4821cb05e
asmith089 <70931098+asmith089@users.noreply.github.com>
2020-11-18 19:45:42 -0500

    new

---
+++ Lab_8/public/js/lab8script.js
@@ -0,0 +1,80 @@
+
+let url = "https://www.reddit.com/r/popular.json";
+let chart = "";
+console.log(url);
+//const fetchPromise = fetch(url);
+
+let settings = { method: "Get" };
+let chartValues = [];
+
+function getRandomInt(min, max) { //getRandomInt(0, listSize)
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+async function getData() {
+    await fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            let listSize = json.data.children.length;
+            console.log(listSize)
+            // Loop to pick 5 random entries
+
+            for (x = 0; x < 5; x++) {
+                let random = getRandomInt(0, listSize);
+                let post = json.data.children[random].data;
+                console.log(post)
+                let subreddit = post.subreddit;
+                let author = post.author;
+                let title = post.title;
+                let ups = post.ups;
+
+                let message = "<b>Subreddit </b>: " + subreddit + 
+                " <b>Author</b>:" + author + " <b>Title</b>:" + 
+                title + " <b>Up votes</b>: " + ups;
+
+                let select = document.getElementById("redditList");
+                select.innerHTML += "<li>" + message + "</li>";
+                /*
+                    Get a random number within the size of the list
+                    Get subreddit, author, title, and ups from record
+                    Set the message to be:
+                        let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+                    Add a new <li> element with the message to the 'redditList' element
+                    Add a data entry to chartValues with author as the label and ups as the y component
+                */
+    
+
+                /*.......*/
+                
+                let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
+                /*.......*/
+                chartValues.push(addToChart);
+            }
+        })
+        .then(values => console.log(chartValues));
+        chart.render(); // Do you need to remove the comments from here in order to get it to work?
+};
+
+async function makeChart() {
+    getData();
+    chart = new CanvasJS.Chart("chartContainer", {
+        title: {
+            text: "Upvotes"
+        },
+
+        data: [     
+            { 
+                type: "column",
+                name: "Popular Reddit",
+                dataPoints: chartValues
+                 // WHAT GOES HERE???
+            }
+        ]
+    });
+    
+    chart.render();
+}
+
+window.onload = makeChart;
